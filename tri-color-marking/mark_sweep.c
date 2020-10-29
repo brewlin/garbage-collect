7637714babb9bbb8e96440ccc4ef6c8caceaad44
brewlin <97404667@qq.com>
2020-10-29 16:21:33 +0800

    [tri-color] #add tri-color-marking

---
+++ tri-color-marking/mark_sweep.c
@@ -0,0 +1,136 @@
+#include "gc.h"
+
+
+//保存了所有申请的对象
+root roots[ROOT_RANGES_LIMIT];
+size_t root_used = 0;
+
+/**
+ * 对该对象进行标记
+ * 并进行子对象标记
+ * @param ptr
+ */
+void gc_mark(void * ptr)
+{
+    GC_Heap *gh;
+    Header *hdr;
+
+    /* mark check */
+    if (!(gh = is_pointer_to_heap(ptr))){
+//      printf("not pointer\n");
+      return;
+    } 
+    if (!(hdr = get_header(gh, ptr))) {
+      printf("not find header\n");
+      return;
+    }
+    if (!FL_TEST(hdr, FL_ALLOC)) {
+      printf("flag not set alloc\n");
+      return;
+    }
+    if (FL_TEST(hdr, FL_MARK)) {
+      //printf("flag not set mark\n");
+      return;
+    }
+
+    /* marking */
+    FL_SET(hdr, FL_MARK);
+//    printf("mark ptr : %p, header : %p\n", ptr, hdr);
+    //进行子节点递归 标记
+    gc_mark_range((void *)(hdr+1), (void *)NEXT_HEADER(hdr));
+}
+/**
+ * 遍历root 进行标记
+ * @param start
+ * @param end
+ */
+void  gc_mark_range(void *start, void *end)
+{
+    void *p;
+
+    //start 表示当前对象 需要释放
+    gc_mark(start);
+    //可能申请的内存 里面又包含了其他内存
+    for (p = start+1; p < end; p++) {
+         //对内存解引用，因为内存里面可能存放了内存的地址 也就是引用，需要进行引用的递归标记
+         gc_mark(*(void **)p);
+    }
+}
+/**
+ * 清除 未标记内存 进行回收利用
+ */
+void     gc_sweep(void)
+{
+    size_t i;
+    Header *p, *pend, *pnext;
+
+    //遍历所有的堆内存
+    //因为所有的内存都从堆里申请，所以需要遍历堆找出待回收的内存
+    for (i = 0; i < gc_heaps_used; i++) {
+        //pend 堆内存结束为止
+        pend = (Header *)(((size_t)gc_heaps[i].slot) + gc_heaps[i].size);
+        //堆的起始为止 因为堆的内存可能被分成了很多份，所以需要遍历该堆的内存
+        for (p = gc_heaps[i].slot; p < pend; p = NEXT_HEADER(p)) {
+            //查看该堆是否已经被使用
+            if (FL_TEST(p, FL_ALLOC)) {
+                //查看该堆是否被标记过
+                if (FL_TEST(p, FL_MARK)) {
+                    DEBUG(printf("解除标记 : %p\n", p));
+                    //取消标记，等待下次来回收，如果在下次回收前
+                    //1. 下次回收前发现该内存又被重新访问了，则不需要清除
+                    //2. 下次回收前发现该内存没有被访问过，所以需要清除
+                    FL_UNSET(p, FL_MARK);
+                }else {
+                    DEBUG(printf("清除回收 :\n"));
+                    gc_free(p+1);
+                }
+            }
+        }
+    }
+}
+/**
+ * 将分配的变量 添加到root 引用,只要是root上的对象都能够进行标记
+ * @param start
+ * @param end
+ */
+void     add_roots(void* obj)
+{
+    roots[root_used].start = obj;
+    roots[root_used].end = obj + CURRENT_HEADER(obj)->size;
+    root_used++;
+
+    if (root_used >= ROOT_RANGES_LIMIT) {
+        fputs("Root OverFlow", stderr);
+        abort();
+    }
+}
+void  gc(void)
+{
+    //垃圾回收前 先从 root 开始 进行递归标记
+    for(int i = 0;i < root_used;i++){
+        gc_mark_range(roots[i].start, roots[i].end);
+    }
+    //标记完成后 在进行 清除 对于没有标记过的进行回收
+    gc_sweep();
+}
+
+/**
+ * 为了防止在中断gc后 其他操作导致之前gc的状态发生异常
+ * 需要引入写入屏障，在更新的时候需要调用处理
+ * @param obj_ptr
+ * @param field
+ * @param new_obj_ptr
+ */
+void write_barrier(void *obj_ptr,void *field,void* new_obj_ptr)
+{
+
+    Header* obj     = CURRENT_HEADER(obj_ptr);
+    Header* new_obj = CURRENT_HEADER(new_obj_ptr);
+    //新对象没有被标记过 将白色涂成灰色
+    if(!IS_MARKED(new_obj)){
+        FL_SET(new_obj,FL_MARK);
+        //push new_obj 到标记栈里面
+    //obj->field = new_obj
+    *(void **)field = new_obj_ptr;
+
+}
---
+++ tri-color-marking/test.c
@@ -0,0 +1,171 @@
+#include "gc.h"
+int clear(){
+    free_list = NULL;
+    for (int i = 0; i <= gc_heaps_used; ++i){
+        gc_heaps[i].size = 0;
+        gc_heaps[i].slot = NULL;
+    }
+    gc_heaps_used = 0;
+
+    for (int j = 0; j <= root_used ; ++j){
+        roots[j].start = NULL;
+        roots[j].end = NULL;
+    }
+    root_used = 0;
+}
+/**
+ * 1. 测试条件保证申请内存保证在TINY_HEAP_SIZE下，因为大于这个数会发生gc
+ * 测试 分配和 释放是否正常
+ */
+void test_malloc_free(size_t req_size){
+    printf("-----------测试内存申请与释放------------\n");
+    printf("-----------***************------------\n");
+    //在回收p1的情况下 p2的申请将复用p1的地址
+    void *p1 = gc_malloc(req_size);
+    gc_free(p1);
+    void *p2 = gc_malloc(req_size);
+    //在上面 p1被释放了，p2 从新申请 会继续从堆的起始位置开始分配 所以 内存地址是一样的
+    assert(p1 == p2);
+    gc_free(p2);
+
+    //在不清除p1的情况下 p2 会申请不同内存
+    p1 = gc_malloc(req_size);
+    p2 = gc_malloc(req_size);
+    if(req_size == TINY_HEAP_SIZE)
+        assert(p1 == p2);
+    else
+        assert(p1 != p2);
+
+
+    printf("-----------   passing     ------------\n\n");
+    clear();
+}
+void test_gc(size_t req_size){
+    printf("-----------测试gc         ------------\n");
+    printf("-----------***************------------\n");
+    void *p1 = gc_malloc(req_size);
+    gc();
+    void *p2 = gc_malloc(req_size);
+    //执行gc后 p1被回收
+    assert(p1 == p2);
+    clear();
+
+    //因为该变量被root引用 所以不会被gc
+    p1 = gc_malloc(req_size);
+    add_roots(p1);
+    gc();
+    p2 = gc_malloc(req_size);
+    assert(p1 != p2);
+
+    printf("-----------   passing     ------------\n\n");
+    clear();
+}
+void test_large_gc(){
+    printf("-----------测试大内存gc         ------------\n");
+    printf("-----------***************------------\n");
+    void *p1 = gc_malloc(TINY_HEAP_SIZE);
+    void *p2 = gc_malloc(TINY_HEAP_SIZE);
+    //因为 req_size 已经达到堆内存了上限了，且无可用内存 会自动执行gc
+    assert(p1 == p2);
+    clear();
+
+    p1 = gc_malloc(TINY_HEAP_SIZE);
+    //将对象加入root
+    add_roots(p1);
+    p2 = gc_malloc(TINY_HEAP_SIZE);
+    //p1 不会被回收
+    //p2 会申请新的内存
+    assert(p1 != p2);
+
+    printf("-----------   passing     ------------\n");
+    clear();
+}
+
+/**
+ * 对引用进行测试
+ */
+void test_reference_gc()
+{
+    printf("-----------测试引用gc         ------------\n");
+    printf("-----------***************------------\n");
+    typedef struct obj{
+        int v;
+        struct obj* left;
+        struct obj* right;
+    }Obj;
+
+    Obj* p   = gc_malloc(sizeof(Obj));
+    p->v     = 10;
+    p->left  = gc_malloc(sizeof(Obj));
+    p->right = gc_malloc(sizeof(Obj));
+    p->left->v = 11;
+    p->right->v = 12;
+
+    //加入root 即使left right 没有加入 但是他们作为 p的子节点引用 会被标记
+    add_roots(p);
+    gc();
+    assert(p->v == 10);
+    assert(p->left->v == 11);
+    assert(p->right->v == 12);
+
+    p   = gc_malloc(sizeof(Obj));
+    p->v     = 10;
+    p->left  = gc_malloc(sizeof(Obj));
+    p->left->v = 11;
+    Obj* left = p->left;
+    //没有加入root 会被清除
+    gc();
+    assert(p->v == 0);
+    assert(p->left == NULL);
+    //子节点也会被清除
+    assert(left->v == 0 );
+
+
+    printf("-----------   passing     ------------\n");
+    clear();
+}
+/**
+ * 测试的时候需要关闭 自动gc
+ */
+void test_malloc_speed(){
+    auto_gc = 0;
+    time_t start,end;
+    start = time(NULL);
+//    for (int i = 0; i < 1000; ++i) {
+//        int size = rand()%90;
+//        void *p = gc_malloc(size);
+//    }
+    for (int i = 0; i < 10000; ++i) {
+        int size = rand()%90;
+        void *p = gc_malloc(size);
+    }
+    void *p = gc_malloc(24);
+    p = gc_malloc(24);
+    p = gc_malloc(24);
+    gc();
+    end = time(NULL);
+    printf("execution seconds:%f\n",difftime(end,start));
+}
+int  main(int argc, char **argv)
+{
+
+    //小内存测试，
+    test_malloc_free(8);
+    clear();
+    //大内存测试
+    test_malloc_free(TINY_HEAP_SIZE);
+    clear();
+    //测试gc
+    test_gc(8);
+    clear();
+    //大内存 无需手动gc 测试
+    test_large_gc();
+    clear();
+    //对象引用测试
+    test_reference_gc();
+    clear();
+
+    //TODO: gc problem
+//    test_malloc_speed();
+    return 0;
+}
