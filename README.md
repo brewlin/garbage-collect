e2a650793e52d858d4a74bf7cbd6fc7bffa79ba8
brewlin <97404667@qq.com>
2020-11-04 10:52:40 +0800

    [gc] #update comment

--- README.md
+++ README.md
@@ -1,29 +1,31 @@
 [toc]
-垃圾回收算法的分析与实现,
+
+垃圾回收算法的分析与实现
+
 对相关算法理论翻译为c代码实现
-# 基础概念
+## 保守式gc
 当前所有gc算法的实现都是基于`保守式gc`:
 ```
 //在gc过程中重要的一个步骤就是要辨别该内存是 指针 or 非指针:
-long  p1 = 0x55555555;
-
-//p2 = 0x5555555;
-void* p2 = gc_malloc();
-obj->value = p1;
-//上面的情况就是p2指针实际是没有被引用的，但是在gc过程中
+void* p2 = gc_malloc(size);
+obj->value = 0x5555555;
+```
+1. 当gc的时候找到`p2`并对其`child`递归搜索标记，找到`value`时误判为指针后标记
+```
 for(child: obj){
-    //这个时候遍历到了value = 0x55555555;的时候误以为是p2指针
-    //结果p2指针本来应该被回收的（因为没有被任何根引用）
-    //但是因为保守式gc 导致被误判了，p2得不到回收
+    //遍历到value成员时 保守判断为一个正确的指针 被标记了
     mark(child);
 }
-
-
-
-
 ```
+2. 但实际上`value`成员只是一个数值，非指针，导致实际指向的内存得不到释放
+3. 要减小这种误判的方式有很多种，现代很多语言都是基于保守式gc，所以肯定有的方式
+```
+1. 对于ruby 来说在编译期间给每个数值进行标记，在运算之前进行还原
+2. 通过将分配的内存进行首地址对齐，然后在辨别指针的时候能够过滤一些
+```
+
 ## heaps 堆
-当前的所有gc实现都会提前向操作系统分配一块大内存`sbrk()`，成为独立管理的内存池，所有内存分配其实都是从这块用户态堆上申请
+当前的所有gc实现都会提前向操作系统分配一块大内存`sbrk()`，成为独立管理的内存池，所有内存分配其实都是从我们维护的内存池中分配
 - gc_malloc 
 ```c
 typedef struct t{
@@ -32,6 +34,8 @@ typedef struct t{
 //无需其他依赖 可动态分配不同类型大小的内存
 T* p = gc_malloc(sizeof(T));
 ```
+
+另外有一个空闲链表机制`free_list`，将所有的`heap`用单链表串联起来，且在用户释放内存的时候会挂到上面进行重复利用
 ## roots 根
 下面这些空间都是根`roots`
 - 寄存器
@@ -42,32 +46,32 @@ T* p = gc_malloc(sizeof(T));
 
 并非只有上面的空间才能成为根,通常情况下对于动态运行时语言来说，都会在程序层面创建一个集合，然后自己来管理分配的对象，实现了根对象的管理
 
+当前所有gc实现都是模拟的root，通过一个全局数组来模拟root对象
 
+## 标记-清除算法
 
-# 标记-清除算法
-
-## [mark-sweep](src/mark-sweep) 
+### [mark-sweep](./src/mark-sweep) 
 基础实现
-## [mark-sweep_multi_free_list](src/mark-sweep_multi_free_list) 
+### [mark-sweep_multi_free_list](./src/mark-sweep-multi-free-list) 
 多链表法实现
 
-# 引用计数算法
-## [reference-count](src/refcount) 
+## 引用计数算法
+### [reference-count](./src/refcount) 
 基础实现
 
-# 复制算法
-## [copying](src/copying) 
+## 复制算法
+### [copying](./src/copying) 
 基础实现
-## [copying_or_mark](src/copying_or_mark) 
+### [copying_or_mark](./src/copying-or-mark) 
 复制算法+标记清除 组合应用实现
 
-# 压缩算法
-## [compact_lisp2](src/compact_lisp2) 
+## 压缩算法
+### [compact_lisp2](./src/compact-lisp2) 
 lisp2 算法
-## [compact_two_finger](src/compact_two_finger) 
+### [compact_two_finger](./src/compact-two-finger) 
 two_finger 算法
 
-# 分代垃圾回收算法
-[generational](src/generational)
-# 增量式-三色标记算法
-[tri-color-marking](src/tri-color-marking)
+## 分代垃圾回收算法
+[generational](./src/generational)
+## 增量式-三色标记算法
+[tri-color-marking](./src/tri-color-marking)
--- header/gc.h
+++ header/gc.h
@@ -15,12 +15,12 @@
  * 同样的也可以通过 ptr-sizeof(header) 拿到header头
  */
 typedef struct header {
-    size_t ref;
-    size_t flags;
-    size_t size;
-    size_t age;
-    struct header *next_free;
-    struct header *forwarding;
+    size_t ref;               //引用计数中使用，其他算法忽略
+    size_t flags;             //marked,remembered,copied
+    size_t size;              //当前内存块大小
+    size_t age;               //分代回收中使用，表示年龄
+    struct header *next_free; //回收链表中使用，指向下一个空闲内存
+    struct header *forwarding;//复制算法中使用, 指向拷贝后的新内存地址
 } Header;
 
 /**
@@ -35,60 +35,63 @@ typedef struct gc_heap {
 } GC_Heap;
 /**
  * 根
+ * 真正意义上的根可以为 全局变量、栈变量、寄存器变量等信息
+ * 这里为了测试通过全局数组来模拟根
  */
 typedef struct root_{
-    void *ptr;
-    void *optr;//这里存储了 用户方变量地址 因为复制完成后需要替换用户引用的变量地址
+    void *ptr; //从heap中申请的内存地址
+    void *optr;//用户栈变量的地址
 }root;
 
 /* marco */
-#define TINY_HEAP_SIZE 4 * 1024              //计算指针 所占内存大小
-#define PTRSIZE ((size_t) sizeof(void *))
-#define HEADER_SIZE ((size_t) sizeof(Header))//堆的上限
-#define HEAP_LIMIT 100000                    //字节对齐 向上取整
-#define ALIGN(x,a) (((x) + (a - 1)) & ~(a - 1))
-#define NEXT_HEADER(x) ((Header *)((size_t)(x+1) + x->size)) //[ [header] x->size [header] x->size ....]
-#define CURRENT_HEADER(x) ((Header *)x - 1)
+#define  TINY_HEAP_SIZE 4 * 1024              //计算指针 所占内存大小
+#define  PTRSIZE ((size_t) sizeof(void *))
+#define  HEADER_SIZE ((size_t) sizeof(Header))//堆的上限
+#define  HEAP_LIMIT 100000                    //字节对齐 向上取整
+#define  ALIGN(x,a) (((x) + (a - 1)) & ~(a - 1))
+#define  NEXT_HEADER(x) ((Header *)((size_t)(x+1) + x->size)) //[ [header] x->size [header] x->size ....]
+#define  CURRENT_HEADER(x) ((Header *)x - 1)
 
-#define ROOT_RANGES_LIMIT 100000
-#define DEBUG(exp) exp
 
 
 /* flags */
-#define FL_ALLOC  0x1
-#define FL_MARK   0x2
-#define FL_COPIED 0x4
-#define FL_REMEMBERED 0x8
-#define FL_SET(x, f) (((Header *)x)->flags |= f)
-#define FL_UNSET(x, f) (((Header *)x)->flags &= ~(f))
-#define FL_TEST(x, f) (((Header *)x)->flags & f)
-#define IS_MARKED(x) (FL_TEST(x, FL_ALLOC) && FL_TEST(x, FL_MARK))
-#define IS_COPIED(x) (FL_TEST(x, FL_ALLOC) && FL_TEST(x, FL_COPIED))
-#define IS_REMEMBERED(x) (FL_TEST(x, FL_ALLOC) && FL_TEST(x, FL_REMEMBERED))
+#define  FL_ALLOC  0x1
+#define  FL_MARK   0x2
+#define  FL_COPIED 0x4
+#define  FL_REMEMBERED 0x8
+#define  FL_SET(x, f) (((Header *)x)->flags |= f)
+#define  FL_UNSET(x, f) (((Header *)x)->flags &= ~(f))
+#define  FL_TEST(x, f) (((Header *)x)->flags & f)
+#define  IS_MARKED(x) (FL_TEST(x, FL_ALLOC) && FL_TEST(x, FL_MARK))
+#define  IS_COPIED(x) (FL_TEST(x, FL_ALLOC) && FL_TEST(x, FL_COPIED))
+#define  IS_REMEMBERED(x) (FL_TEST(x, FL_ALLOC) && FL_TEST(x, FL_REMEMBERED))
 
 /* public api */
 void     gc(void);                     //执行gc 垃圾回收
-void     gc_init(size_t heap_size);
+void     gc_init(size_t heap_size);    //有些算法不需要动态扩充堆，那么就需要提前初始化堆
 void     gc_free(void *ptr);           //回收内存
-void*    gc_malloc(size_t req_size);   //从堆缓存中申请一份内存
-Header*  gc_grow(size_t req_size);     //某些算法在内存不够的时候会扩充堆
+void*    gc_malloc(size_t req_size);   //内存分配
 GC_Heap* is_pointer_to_heap(void *ptr);//获取指针对应的堆首地址
 GC_Heap* is_pointer_to_space(void *ptr,size_t i);
 //安全的获取header头，可以通过内存段来定位该header
 // 因为如果ptr 刚好不在 header+1处的话 无法通过(ptr- sizeof(Header)) 来获取header地址
 Header*  get_header(GC_Heap *gh, void *ptr);
-void     add_roots(void* o_ptr);
-void     add_heaps(size_t req_size);
+void     add_heaps(size_t req_size);   //扩充堆
+Header*  gc_grow(size_t req_size);     //某些算法在内存不够的时候会扩充堆
 
 
 /* global variable */
-extern Header *free_list;
-extern GC_Heap gc_heaps[HEAP_LIMIT];
-extern size_t gc_heaps_used;
-extern int auto_gc;
-extern int auto_grow;
+extern   Header *free_list;
+extern   GC_Heap gc_heaps[HEAP_LIMIT];
+extern   size_t gc_heaps_used;
+extern   int auto_gc;                  //测试的时候 有时候需要关闭内存不够时执行gc
+extern   int auto_grow;                //测试的时候 有时候需要关闭内存不够时扩充堆
 
 
-extern root roots[ROOT_RANGES_LIMIT];
-extern size_t root_used;
+/* roots */
+#define  ROOT_RANGES_LIMIT 100000
+#define  DEBUG(exp) exp
+extern   root roots[ROOT_RANGES_LIMIT];
+extern   size_t root_used;
+void     add_roots(void* o_ptr);       //模拟根，真正意义的根来自全局、栈、寄存器等、这里模拟用数组实现
 #endif
